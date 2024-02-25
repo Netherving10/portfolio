6b98f2817c28e3a04cb369b2acd8e3bfbcae1f83
Максим Даньшин <netherving.80@gmail.com>
2024-02-25 21:32:33 +0300

    add apps descroptions, images, urls

--- main.py
+++ main.py
@@ -9,25 +9,31 @@ CONTENT_1 = '''
 '''
 CONTENT_2 = "Below yo can find some of the apps I have built in Python. Feel free to contact me!"
 
-streamlit.set_page_config(layout='wide')
+streamlit.set_page_config(layout="wide")
 col1, col2 = streamlit.columns(2)
 
 with col1:
-    streamlit.image('dru.jpg')
+    streamlit.image("dru.jpg")
 
 with col2:
-    streamlit.title('Netherving')
+    streamlit.title("Netherving")
     streamlit.info(CONTENT_1)
 
 streamlit.write(CONTENT_2)
 
-col3, col4 = streamlit.columns(2)
-df = pandas.read_csv('data.csv', sep=';')
+col3, empty_col, col4 = streamlit.columns([1.5, 0.5, 1.5])
+df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
-        streamlit.header(row['title'])
+        streamlit.header(row["title"])
+        streamlit.write(row["description"])
+        streamlit.image("images/" + row["image"])
+        streamlit.write(f"[Souce code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
-        streamlit.header(row['title'])
+        streamlit.header(row["title"])
+        streamlit.write(row["description"])
+        streamlit.image("images/" + row["image"])
+        streamlit.write(f"[Souce code]({row['url']})")
