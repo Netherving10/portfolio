28063ffa019a834bf439f93d12c472c5c88a8c1e
Максим Даньшин <netherving.80@gmail.com>
2024-02-25 21:09:24 +0300

    add render the titles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 
 CONTENT_1 = '''
@@ -8,14 +9,25 @@ CONTENT_1 = '''
 '''
 CONTENT_2 = "Below yo can find some of the apps I have built in Python. Feel free to contact me!"
 
-streamlit.set_page_config(layout="wide")
+streamlit.set_page_config(layout='wide')
 col1, col2 = streamlit.columns(2)
 
 with col1:
-    streamlit.image("dru.jpg")
+    streamlit.image('dru.jpg')
 
 with col2:
-    streamlit.title("Netherving")
+    streamlit.title('Netherving')
     streamlit.info(CONTENT_1)
 
 streamlit.write(CONTENT_2)
+
+col3, col4 = streamlit.columns(2)
+df = pandas.read_csv('data.csv', sep=';')
+
+with col3:
+    for index, row in df[:10].iterrows():
+        streamlit.header(row['title'])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        streamlit.header(row['title'])
