c226371d78dd1fae9a200ec34c6a7d85edb87a86
Максим Даньшин <netherving.80@gmail.com>
2024-02-29 00:18:38 +0300

    fix bugs

--- Home.py
+++ Home.py
@@ -1,4 +1,4 @@
-import streamlit
+import streamlit as st
 import pandas
 
 
@@ -9,31 +9,31 @@ CONTENT_1 = '''
 '''
 CONTENT_2 = "Below yo can find some of the apps I have built in Python. Feel free to contact me!"
 
-streamlit.set_page_config(layout="wide")
-col1, col2 = streamlit.columns(2)
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
 
 with col1:
-    streamlit.image("dru.jpg")
+    st.image("dru.jpg")
 
 with col2:
-    streamlit.title("Netherving")
-    streamlit.info(CONTENT_1)
+    st.title("Netherving")
+    st.info(CONTENT_1)
 
-streamlit.write(CONTENT_2)
+st.write(CONTENT_2)
 
-col3, empty_col, col4 = streamlit.columns([1.5, 0.5, 1.5])
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
-        streamlit.header(row["title"])
-        streamlit.write(row["description"])
-        streamlit.image("images/" + row["image"])
-        streamlit.write(f"[Souce code]({row['url']})")
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Souce code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
-        streamlit.header(row["title"])
-        streamlit.write(row["description"])
-        streamlit.image("images/" + row["image"])
-        streamlit.write(f"[Souce code]({row['url']})")
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Souce code]({row['url']})")
